d539bfe5f5b4c4b503c29cd4374f82957756960e
Umarbek1706 <rashidovumarbek1706@gmail.com>
2025-08-27 22:58:20 +0500

    avfg

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styles from "./Card.module.css";
+import styles from "./card.module.css";
 
 function Card() {
   const [products, setProducts] = useState([]);
